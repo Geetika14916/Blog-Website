9aac37b0e7f11b6472a522c177af646c565ab88f
Geetika14916 <shekhawat.geetika@gmail.com>
2024-12-17 22:18:09 +0530

    env variables
    converted port and mongo url into environment variables for using cloud service

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routers/user");
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 const mongoose = require("mongoose");
 const cookieparser = require("cookie-parser");
 const { checkAuthenticationCookie } = require("./middlewares/authentication");
 const blogRoute = require("./routers/blog");
 const Blog = require("./models/blog");
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/Blogify")
-  .then(console.log("MongoDb connected"));
+mongoose.connect(process.env.MONGO_URL).then(console.log("MongoDb connected"));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
